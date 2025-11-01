a94b2ef3faccf604a62dfcf556cc49274fbb6dff
Oreoluwa Akinola <akinolaoreoluwa@gmail.com>
2025-11-01 09:44:02 +0100

    implemented loan logic

--- coop.cs
+++ coop.cs
@@ -5,29 +5,35 @@ namespace Cooperative;
 
 public class Cooperator
 {
+    //properties
     public string Name { get; set; }
     public string Factory {get; set;}
     public int CoopNumber { get; set; }
     public int StaffNumber { get; set; }
     public int MonthlyContribution { get; set; }
     public int BuildingFundBalance { get; set; }
-    public int InvestmentBalance = 0;
-    public int SharesBalance = 0;
-    public int SavingsBalance = 0;
+    public int InvestmentBalance { get; set; }
+    public int SharesBalance { get; set; }
+    public int SavingsBalance { get; set; }
+    public int LoanBalance { get; set; }
+    //constants
     public const int DevelopmentLevy = 300;
     public const int MaxBuildingFund = 30000;
     public const int MaxInvestment = 80000;
     public const int MaxShares = 30000;
     public const int EntranceFee = 3500;
+
+    //balances
     public int CooperativeDevLevyBal;
     public int CooperativeEntranceFeeBal;
+    
 
     //public List<int> CoopMembers = new List<int>();
 
     public int GenerateCoopNumber()
     {
         var rand = new Random();
-        return rand.Next(1, 700);
+        return rand.Next(1, 3000);
     }
 
     public int GenerateStaffNumber()
@@ -36,6 +42,7 @@ public class Cooperator
         return rand.Next(10000, 90000);
     }
 
+    //initialize cooperator
     public Cooperator(string name, int contribution, string factory, int bf_amount, int inv_amount, int shares_amount)
     {
         Name = name;
@@ -47,6 +54,7 @@ public class Cooperator
         FirstSaving(contribution, bf_amount, shares_amount, inv_amount);
     }
 
+    //update savings
     public void SaveMoney(int bf_amount, int inv_amount, int shares_amount, int savings_amount, int monthly_contribution)
     {
         if (monthly_contribution != bf_amount + inv_amount + savings_amount + DevelopmentLevy)
@@ -89,8 +97,9 @@ public class Cooperator
 
         SavingsBalance += savings_amount;
     }
-    
-    public void FirstSaving(int monthly, int bf_amount, int shares_amount, int inv_amount)
+
+    //initialize account with savings
+    public void FirstSaving(int monthly, int bf_amount, int shares_amount, int inv_amount) //use nullable parameters
     {
         if (monthly != EntranceFee + DevelopmentLevy + bf_amount + shares_amount + inv_amount)
         {
@@ -104,7 +113,16 @@ public class Cooperator
             SharesBalance += shares_amount;
             InvestmentBalance += inv_amount;
         }
-
-       
+    }
+    
+    public double GetLoan(int amount)
+    {
+        double interest = LoanInterest(amount);
+        return interest + amount;
+        //logic to implement - must have spent 6 months in cooperative, 
+    }
+    public double LoanInterest(int amount)
+    {
+        return 0.1 * amount;
     }
 }
